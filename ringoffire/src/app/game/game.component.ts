089b41d21b91503e32b46c6bf92fef67969a0e2e
SABRINA FRITZ <SABRINA_FRITZ@GMX.NET>
2025-04-09 16:14:50 +0200

    take random cards work

--- ringoffire/src/app/game/game.component.ts
+++ ringoffire/src/app/game/game.component.ts
@@ -1,17 +1,44 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { Game } from '../../models/game';
 
 @Component({
   selector: 'app-game',
-  imports: [CommonModule],
+  imports: [CommonModule,],
   templateUrl: './game.component.html',
   styleUrl: './game.component.scss'
 })
+
 export class GameComponent {
   pickCardAnimation = false;
-  
+  currentCard: string = "";
+  game!: Game;
+
+  constructor() {
+    
+  }
+
+  ngOnInit() {
+    this.newGame();
+  }
+
+  newGame() {
+    this.game = new Game();
+    console.log(this.game);
+    
+  }
+
   takeCard() {
-    this.pickCardAnimation = true;
+    if (!this.pickCardAnimation) {
+      
+      this.pickCardAnimation = true;
+      this.currentCard = this.game.stack.pop() || '';
+      console.log('currentCard:', this.currentCard);
+
+      setTimeout(() => {
+        this.pickCardAnimation = false;
+      },1500)
+    }
   }
 
 }
