f6cb718071c81f862449e4bed87470e7833fa10c
SABRINA FRITZ <SABRINA_FRITZ@GMX.NET>
2025-04-15 16:54:11 +0200

    connection with firebase

--- ringoffire/src/app/app.config.ts
+++ ringoffire/src/app/app.config.ts
@@ -1,15 +1,44 @@
+// import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
+// import { provideRouter } from '@angular/router';
+// // import { MatButtonModule } from '@angular/material/button';
+// // import { MatIconModule } from '@angular/material/icon';
+// // import { importProvidersFrom } from '@angular/core';
+
+// import { routes } from './app.routes';
+
+// export const appConfig: ApplicationConfig = {
+//   providers: [
+//     provideZoneChangeDetection({ eventCoalescing: true }), 
+//     provideRouter(routes),
+//     // importProvidersFrom(MatButtonModule, MatIconModule)
+//   ]
+// };
+
+
 import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
-// import { MatButtonModule } from '@angular/material/button';
-// import { MatIconModule } from '@angular/material/icon';
-// import { importProvidersFrom } from '@angular/core';
-
 import { routes } from './app.routes';
+
+// Firebase:
+import { provideFirebaseApp, initializeApp } from '@angular/fire/app';
+import { provideAuth, getAuth } from '@angular/fire/auth';
+import { provideFirestore, getFirestore } from '@angular/fire/firestore';
+
+const firebaseConfig = {
+  apiKey: "AIzaSyA4E3L43sW-_gwG5d9qXjqgIpSu83i7fbw",
+  authDomain: "ring-of-fire-a7e2a.firebaseapp.com",
+  projectId: "ring-of-fire-a7e2a",
+  storageBucket: "ring-of-fire-a7e2a.appspot.com",
+  messagingSenderId: "986575367970",
+  appId: "1:986575367970:web:965bf081e726d278293f7b"
+};
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideZoneChangeDetection({ eventCoalescing: true }), 
     provideRouter(routes),
-    // importProvidersFrom(MatButtonModule, MatIconModule)
+    provideFirebaseApp(() => initializeApp(firebaseConfig)),
+    provideAuth(() => getAuth()),
+    provideFirestore(() => getFirestore()),
   ]
 };
