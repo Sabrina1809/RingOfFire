71d49351c913c9d6c1a5cb7f385dc5b95c76c196
SABRINA FRITZ <SABRINA_FRITZ@GMX.NET>
2025-04-20 09:14:14 +0200

    09 - start new game

--- ringoffire/src/app/start-screen/start-screen.component.ts
+++ ringoffire/src/app/start-screen/start-screen.component.ts
@@ -1,23 +1,66 @@
+// import { CommonModule } from '@angular/common';
+// import { Component } from '@angular/core';
+// import { Router } from '@angular/router';
+// import { Firestore } from '@angular/fire/firestore';
+
+// @Component({
+//   selector: 'app-start-screen',
+//   imports: [CommonModule],
+//   templateUrl: './start-screen.component.html',
+//   styleUrl: './start-screen.component.scss'
+// })
+// export class StartScreenComponent {
+
+//   constructor(
+//     private router: Router,
+//      private firestore: Firestore
+//   ) {
+    
+//   }
+
+//   newGame() {
+//     console.log('start game');
+//     let game = new Game();
+//     this.firestore.collection('games').add(game.toJson()).then((gameInfo: any) => {
+//       console.log(gameInfo);
+      
+//     })
+//     this.router.navigateByUrl('/game/DmxF3fGtpx3ieFBBFjRN')
+//   }
+// }
+
+
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { Router } from '@angular/router';
+import { Firestore, collection, addDoc } from '@angular/fire/firestore';
+import { Game } from '../../models/game'; // Passe den Pfad ggf. an!
 
 @Component({
   selector: 'app-start-screen',
+  standalone: true,
   imports: [CommonModule],
   templateUrl: './start-screen.component.html',
-  styleUrl: './start-screen.component.scss'
+  styleUrls: ['./start-screen.component.scss']
 })
 export class StartScreenComponent {
 
-  constructor(private router: Router) {
-    
-  }
+  constructor(
+    private router: Router,
+    private firestore: Firestore
+  ) {}
 
-  newGame() {
+  async newGame() {
     console.log('start game');
-    this.router.navigateByUrl('/game/DmxF3fGtpx3ieFBBFjRN')
+    const game = new Game();
+    const gamesCollection = collection(this.firestore, 'games');
+
+    try {
+      const gameRef = await addDoc(collection(this.firestore, 'games'), game.toJson());
+      console.log('Game created with ID:', gameRef.id);
+      this.router.navigateByUrl('/game/' + gameRef.id);
+    } catch (error) {
+      console.error('Error creating game:', error);
+    }
   }
 }
-
-
